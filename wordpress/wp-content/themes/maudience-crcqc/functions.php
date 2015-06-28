26ade8791ebffe6f6351a019c6f97e3fb4f72bfc
Pete Lower <pete.lower@gmail.com>
2015-06-28 10:00:46 -0700

    add header top right widget

--- wordpress/wp-content/themes/maudience-crcqc/functions.php
+++ wordpress/wp-content/themes/maudience-crcqc/functions.php
@@ -59,6 +59,15 @@ require_once('lib/maudience-contactinfo.php');
         // ) );
 
         register_sidebar( array(
+            'name' => 'Header Top Right',
+            'id' => 'header-top-right',
+            'before_widget' => '<div id="header-top-right" class="header-top-right">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="rounded">',
+            'after_title' => '</h2>',
+        ) );
+
+        register_sidebar( array(
             'name' => 'Home Top Center Full',
             'id' => 'home-top-centerfull',
             'before_widget' => '<div id="home-top-centerfull" class="home-top-centerfull">',
